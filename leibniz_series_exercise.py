4f16f8459b01514784bf4d0b6d33964c299f32b0
amitr202 <amitira100@gmail.com>
2025-11-07 00:27:43 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,2 +1,12 @@
 def approximate_pi(n_terms):
-    pass # replace pass with your code
+  list_of_numbers = []
+  sign = 1
+  denom = 1
+  for i in range(n_terms):
+    num = sign/denom
+    list_of_numbers.append(num)
+    sign *= -1 #sign = sign *(-1)
+    denom += 2
+  pi = sum(list_of_numbers)
+  pi *= 4
+  print(pi)
